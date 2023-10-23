0d49f04380b5bda1bffe4cae51da321e1f225503
Timerlan01 <alimbekovtimerlan@gmail.com>
2023-10-23 11:22:57 +0600

    Добавьте файлы проекта.

---
+++ Module08HM/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module08HM
+{
+    class Program
+    {
+        static void Main()
+        {
+            int size = 5; // Размер массива
+            SquaringIndexer indexer = new SquaringIndexer(size);
+
+            for (int i = 0; i < size; i++)
+            {
+                indexer[i] = i; // Устанавливаем значение, которое будет возводиться в квадрат
+            }
+
+            for (int i = 0; i < size; i++)
+            {
+                Console.WriteLine($"Element {i}: {indexer[i]}");
+            }
+        }
+    }
+
+}
---
+++ Module08HM/SquaringIndexer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module08HM
+{
+    class SquaringIndexer
+    {
+        private double[] array;
+
+        public SquaringIndexer(int size)
+        {
+            array = new double[size];
+        }
+
+        public double this[int index]
+        {
+            get
+            {
+                if (index >= 0 && index < array.Length)
+                {
+                    return array[index];
+                }
+                else
+                {
+                    throw new IndexOutOfRangeException();
+                }
+            }
+            set
+            {
+                if (index >= 0 && index < array.Length)
+                {
+                    array[index] = value * value; // Возводим значение в квадрат перед установкой
+                }
+                else
+                {
+                    throw new IndexOutOfRangeException();
+                }
+            }
+        }
+    }
+}
